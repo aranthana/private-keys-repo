b9d401bdc95df8dac42574f39cdec9df5398a885
Aranya <aru2191@gmail.com>
2022-07-10 22:17:15 +1000

    Removed unused constants

--- src/main/java/com/privatekeys/repository/util/Constants.java
+++ src/main/java/com/privatekeys/repository/util/Constants.java
@@ -2,8 +2,6 @@ package com.privatekeys.repository.util;
 
 public class Constants {
 
-    public static final String SPEC = "prime256v1";
     public static final String ALGORITHM = "SHA3-256withECDSA";
-    //public static final String ALGORITHM_SHA256 = "SHA256withECDSA";
-    public static final String EC = "EC";
+
 }
